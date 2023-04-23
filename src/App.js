d92116bfe219d730342f493ebb2f48daa98892d9
unknown <forkan.saadaoui@gmail.com>
2023-04-23 18:43:46 +0100

    footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Qualification from "./components/qualification/Qualification";
@@ -19,6 +20,7 @@ function App() {
         <Qualification/>
         <Contact/>
       </main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>
+        &#169; Forkan. All rigths reserved
+      </p>
+    </div>
+  )
+}
+
+export default Footer
